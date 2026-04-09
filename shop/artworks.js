6fa5c23bf2db8aa6d4fd7952b14141f3a32c865b
Rabbitflower <rabbitflower-rabiko@proton.me>
2026-04-09 15:08:14 +0900

    27

--- shop/artworks.js
+++ shop/artworks.js
@@ -1,28 +1,28 @@
 const artworks = [
   {
     title: "Rabbitflower Doll#94 [water color]",
-    price: "",
+    price: "8300",
     image: "./images/094.jpg",
-    link: "",
+    link: "https://www.paypal.com/ncp/payment/ELVHS843KVW7L",
     soldout: true
   },
   {
     title: "Rabbitflower Doll#93 [water color]",
-    price: "",
+    price: "6800",
     image: "./images/093.jpg",
-    link: "",
+    link: "https://www.paypal.com/ncp/payment/27SLYCT98JDTY",
     soldout: true
   },
   {
     title: "Rabbitflower Doll#92 [water color]",
-    price: "",
+    price: "8200 yen",
     image: "./images/092.jpg",
-    link: "",
+    link: "https://www.paypal.com/ncp/payment/GBNMA3RLLBNLQ",
     soldout: true
   },
   {
     title: "Rabbitflower Doll#91 [water color]",
-    price: "¥6800",
+    price: "6800 yen",
     image: "./images/091.jpg",
     link: "https://www.paypal.com/ncp/payment/CEBVE2U6N9EV2",
     soldout: false
